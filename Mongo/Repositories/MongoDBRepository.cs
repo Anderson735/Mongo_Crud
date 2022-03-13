c9e3a2b045d1e26392958f36425a0ec0a8096b62
Anderson <andersonmendezmarin@gmail.com>
2022-03-13 14:22:41 -0500

    Agregar archivos de proyecto.

---
+++ Mongo/Repositories/MongoDBRepository.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mongo.Repositories
+{
+    public class MongoDBRepository
+    {
+        public MongoClient client;
+
+        public IMongoDatabase db;
+
+        public MongoDBRepository()
+        {
+            client = new MongoClient("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb");
+
+            db = client.GetDatabase("Inventory");
+        }
+    }
+}
